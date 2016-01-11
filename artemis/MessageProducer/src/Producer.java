3834e19c115c22e8f98c0855768de646fc1a26b5
Tobias Schaber <tobias.schaber@codecentric.de>
2016-01-11 17:03:32 +0100

    commented jar files

--- artemis/MessageProducer/src/Producer.java
+++ artemis/MessageProducer/src/Producer.java
@@ -2,12 +2,10 @@
 
 public class Producer {
 	
-	
-	
 	public static void main(String[] args) throws Exception {
 	
 		System.out.println("Starting");
-		
+	
 		ProducerRunnable runner = new ProducerRunnable();
 		
 		Thread thread = new Thread(runner);
--- artemis/MessageProducer/src/ProducerRunnable.java
+++ artemis/MessageProducer/src/ProducerRunnable.java
@@ -29,7 +29,7 @@ public class ProducerRunnable implements Runnable {
 		
 	public ProducerRunnable() throws Exception {
 	
-	
+		/* create a UDP discovery group */
 		UDPBroadcastEndpointFactory udpCfg = new UDPBroadcastEndpointFactory();
 		udpCfg
 			.setGroupAddress("231.7.7.7")
@@ -39,15 +39,21 @@ public class ProducerRunnable implements Runnable {
 		groupConfiguration.setBroadcastEndpointFactory(udpCfg);
 		
 		ActiveMQConnectionFactory cf = ActiveMQJMSClient.createConnectionFactoryWithHA(groupConfiguration, JMSFactoryType.CF);
+		
+		/* these properties are required, otherwise the failover will NOT work !! */
 		cf.setRetryInterval(1000);
 		cf.setRetryIntervalMultiplier(1.5);
 		cf.setMaxRetryInterval(60000);
 		cf.setReconnectAttempts(1000);
 		
+		/* wait a short period for discovery */
 		System.out.println("waiting for discovery");
 		Thread.sleep(2000);
 		
+		/* create a connection */
 		connection = cf.createConnection("admin", "admin");
+		
+		/* add a failover listener to see the failover steps */
         ((ActiveMQConnection) connection).setFailoverListener(new FailoverListenerImpl());
         
 		connection.start();
@@ -65,34 +71,40 @@ public class ProducerRunnable implements Runnable {
 			MessageProducer producer = session.createProducer(queue);
 			MessageConsumer consumer = session.createConsumer(queue);
 			
+			/* endless repetition of send-receive */
 			while(true) {
 				
 				String messageText = "msg_" + counter;
 				
 				System.out.println("sending message: " + messageText);
 				
+				/* create a text message and send it to the broker */
 				TextMessage message = session.createTextMessage(messageText);	
 				producer.send(message);
 					
 				System.out.println("receiving message...");	
 				
+				/* wait a short time */
 				Thread.sleep(1000);
 				
+				/* read the message from the server */
 				TextMessage received = (TextMessage)consumer.receive();
 				
 				
 //				--------------------------------------------------------------------------------------------------------------
 				
-//				System.out.println("recived: " + received);
-				
+				/* extract the content of the received message */
 				String resp = received.getText();
 				
 				System.out.println(resp);
 				
+				/* compare the received message with the expected one */
 				if(resp.equals(messageText)) {
 					System.out.println("OK");
 				} else {
 					System.out.println(messageText);
+					
+					/* exit if message is wrong */
 					throw new Exception("RECEIVED WRONG MESSAGE!!!");
 					
 				}
@@ -103,11 +115,14 @@ public class ProducerRunnable implements Runnable {
 		} catch(Exception e) {
 			e.printStackTrace();
 		} finally {
-			
 			close();
 		}
 	}
 	
+	
+	/**
+	 * close connection and stuff
+	 */
 	public void close() {
 		try {
 			connection.close();
@@ -118,10 +133,16 @@ public class ProducerRunnable implements Runnable {
 	}
 
 	
-	   private static class FailoverListenerImpl implements FailoverEventListener {
+	/**
+	 * Failover Listener which will alert all failover events
+	 * @author tobias
+	 *
+	 */
+	private static class FailoverListenerImpl implements FailoverEventListener {
 
-		      public void failoverEvent(FailoverEventType eventType) {
-		         System.out.println("Failover event triggered :" + eventType.toString());
-		      }
-		   }
+		public void failoverEvent(FailoverEventType eventType) {
+			System.out.println("Failover event triggered :" + eventType.toString());
+		}
+	}
+	
 }
